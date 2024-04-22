7141e5543a3de0001f924e23ddd080454a091c47
miracle <miracle1695@gmail.com>
2024-04-22 15:57:59 +0300

    Add comments

--- frontend-gateway/__init__.py
+++ frontend-gateway/__init__.py
@@ -1,17 +1,23 @@
 from flask import Flask
-from flask_login import UserMixin
+from flask_login import UserMixin  #Usermixin provides default user authentication methods
 
+# Class that represents a user in the application, extending Usermixin 
 class User(UserMixin):
+    # Initialize the User object with ID, name, and active status
     def __init__(self, id,  name="", active=True):
         self.name = name
         self.id = id
-        self.active = active
+        self.active = active # Whether the user is active
     
+    # Method to check if the user is active 
     def is_active(self):
-        return self.active
+        return self.active # Return True if the user is active
+
+    # MEthod to check if the user is anonymous (always False)
     def is_anonymous(self):
         return False
-    def is_authenticated(self):
+    
+    def is_authenticated(self): # always True
         return True
 
 def create_app():
--- frontend-gateway/auth.py
+++ frontend-gateway/auth.py
@@ -1,25 +1,31 @@
 import os, requests
 
-AUTH_SVC = os.getenv('AUTH_SVC', 'localhost') # user-service
-AUTH_PORT = os.getenv('AUTH_PORT', 5000) # 5000
+# GLOBAL ENV
+AUTH_SVC = os.getenv('AUTH_SVC')
+AUTH_PORT = os.getenv('AUTH_PORT') # 5000
 
+# Function to handle login
 def login(request):
+    # Gather credentials from the request form
     auth = {
         'email': request.form.get('email'),
         'password': request.form.get('password')
     }
+    # If credentials are missing, return an error message with status code 401 (Unathorized)
     if not auth:
         return "missing credentials", 401
-    #basic_auth = (auth['username'], auth['password'])
     
+    # Make a POST request to the authentication service's login endpoint with the credentials
     response = requests.post(
         f"http://{AUTH_SVC}:{AUTH_PORT}/login", data=auth
     )
     
-
+    # Return the response text and status code from the login request
     return response.text, response.status_code
 
+# Function for handle signup
 def signup(request):
+    # Gather signup information from the request form
     auth = {
         'email': request.form.get('email'),
         'name': request.form.get('name'),
@@ -32,5 +38,5 @@ def signup(request):
         f"http://{AUTH_SVC}:{AUTH_PORT}/signup", data=auth
     )
     
-
+    # Return the response text and status code from the signup request
     return response.text, response.status_code
